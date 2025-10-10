1a783d3d91e0e94f1f36161b78543d7a5c86e60a
Teaching2504 <you@example.com>
2025-10-10 16:54:03 +0700

    update

---
+++ saleapp/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import dao
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def index():
+    q = request.args.get("q")
+    cate_id = request.args.get("cate_id")
+    cates = dao.load_category()
+    prods= dao.load_product(q=q, cate_id=cate_id)
+    return render_template("index.html", cates=cates, prods=prods)
+
+if __name__ == "__main__":
+    app.run(debug=True)
